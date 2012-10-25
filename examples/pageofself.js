33ab5cc5299928901163897dd852df61c68cd88b
Thorsten Lorenz <thlorenz@gmx.de>
2012-10-24 22:53:18 -0400

    example updated

--- examples/pageofself.js
+++ examples/pageofself.js
@@ -2,7 +2,9 @@
 
 var peacock  =  require('..')
   , fs       =  require('fs')
+  , path     =  require('path')
   , exec     =  require('child_process').exec
+  , styles   =  path.join(__dirname, '..', 'styles')
   , code     =  fs.readFileSync(__filename, 'utf-8')
   , htmlFile =  './pageofself.html'
   ;
@@ -29,7 +31,7 @@ var  html = [
       , '<head>'
       , '   <meta http-equiv="content-type" content="text/html; charset=utf-8"/>'
       , '   <title>Page of Self</title>'
-      , ' <link rel="stylesheet" href="styles/tango.css" type="text/css" media="screen" charset="utf-8" />'
+      , ' <link rel="stylesheet" href="' + styles + '/tango.css" type="text/css" media="screen" charset="utf-8" />'
       , '</head>'
       , '<body>'
       , highlightedCode
