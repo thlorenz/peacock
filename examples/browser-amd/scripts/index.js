b638fad81036a276ed9924d3a04c249488b3ce1f
Thorsten Lorenz <thlorenz@gmx.de>
2012-10-29 22:20:50 -0400

    amd example demos toggling line numbers

--- examples/browser-amd/scripts/index.js
+++ examples/browser-amd/scripts/index.js
@@ -3,6 +3,7 @@ require(['jquery', 'peacock'], function ($, peacock) {
     , $result        =  $('.result')
     , $styleSelector =  $('#style-selector')
     , $styles        =  $('#style-link')
+    , $linenumbers   =  $('#linenumbers')
     , stylesPath     =  '../../styles'
     ;
 
@@ -16,7 +17,7 @@ require(['jquery', 'peacock'], function ($, peacock) {
 
     try {
       code = $code.val();
-      var result = peacock.highlight(code, { linenos: true });
+      var result = peacock.highlight(code, { linenos: $linenumbers.attr('checked') });
       $result.append(result);
     } catch (e) {
       var raw = [ 
@@ -37,7 +38,10 @@ require(['jquery', 'peacock'], function ($, peacock) {
   }
 
   $code.val(peacock.highlight.toString());
+  $linenumbers.attr('checked', 'checked');
+
   $code.on('input propertychange', go);
+  $linenumbers.on('change', go);
 
   $styleSelector.on('change', function (event) {
     var el = $styleSelector[0];
