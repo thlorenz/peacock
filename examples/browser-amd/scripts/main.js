a23634a8a8176d27390c7196ecb77eb3a4516d74
Thorsten Lorenz <thlorenz@gmx.de>
2018-05-21 14:14:16 -0600

    examples: fixing javascript links

--- examples/browser-amd/scripts/main.js
+++ examples/browser-amd/scripts/main.js
@@ -2,8 +2,8 @@ requirejs.config ({
   // NOTE: In order to simplify this example, we pull the dependencies from the web. In a real app they should be local.
   paths: {
       'jquery'  :  'http://ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min'
-    , 'esprima' :  'https://raw.github.com/ariya/esprima/master/esprima'
-    , 'redeyed' :  'https://raw.github.com/thlorenz/redeyed/master/redeyed'
+    , 'esprima' :  'https://cdnjs.cloudflare.com/ajax/libs/esprima/2.7.3/esprima.min'
+    , 'redeyed' :  'https://rawgit.com/thlorenz/redeyed/master/redeyed'
     , 'peacock' :  '../../../peacock-browser'
   }
 });
