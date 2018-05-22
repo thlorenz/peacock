7c67d8c9aa25ad6f9e9759144f24e78da67497f6
Thorsten Lorenz <thlorenz@gmx.de>
2018-05-22 12:47:49 -0600

    examples: updating examples
    - adding commonjs example
    - fixing amd example to include jsx support
    - removing scripts example since newest esprima is not attaching a global
      and no one writes code like that anymore anyhow

--- examples/browser-amd/scripts/main.js
+++ examples/browser-amd/scripts/main.js
@@ -5,7 +5,7 @@ requirejs.config({
   // NOTE: In order to simplify this example, we pull the dependencies from the web. In a real app they should be local.
   paths: {
       'jquery'  :  'http://ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min'
-    , 'esprima' :  'https://cdnjs.cloudflare.com/ajax/libs/esprima/2.7.3/esprima.min'
+    , 'esprima' :  'https://cdn.jsdelivr.net/npm/esprima@4.0.0/dist/esprima.min'
     , 'redeyed' :  'https://rawgit.com/thlorenz/redeyed/master/redeyed'
     , 'peacock' :  '../../../peacock-browser'
   }
